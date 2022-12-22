80085db15cc69a0c233e6c1bc6560eafd71b2731
Hanna Taniguchi <hanely.menezes@gmail.com>
2022-12-21 23:52:28 -0300

    call back producer

--- kafka-ms-order/src/main/java/com/course/kafka/broker/producer/OrderProducer.java
+++ kafka-ms-order/src/main/java/com/course/kafka/broker/producer/OrderProducer.java
@@ -1,16 +1,43 @@
 package com.course.kafka.broker.producer;
 
 import com.course.kafka.broker.message.OrderMessage;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import org.apache.kafka.clients.producer.Callback;
+
 @Service
 public class OrderProducer {
+    // we will just do some log for callback
+    private static final Logger LOG = LoggerFactory.getLogger(OrderProducer.class);
+
+    //var producer = new KafkaProducer<String, OrderMessage>;
+
+    @Autowired
+    private KafkaProducer<String, OrderMessage> kafkaProducer;
+
     @Autowired
     private KafkaTemplate<String, OrderMessage> kafkaTemplate;
 
-    public void publish(OrderMessage message){
-        kafkaTemplate.send("t-commodity-order", message.getOrderNumber(), message);
+
+    public void publish(OrderMessage message) {
+        kafkaProducer.send(, (data,ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            LOG.info("Order {}, item {}, published successfully", message.getOrderNumber(), message.getItemName());
+        });
+        // Callback --> return success or failured message
+        // publishing might not always smooth, broker might not be available, or network having latency
+        // in such case, we can add callback to Future object to handle publishing success or failure
+        //By using callback, we can log each publish result and find out, maybe 99,95% message is published,
+        // while the other is error.
+
+
     }
 }
