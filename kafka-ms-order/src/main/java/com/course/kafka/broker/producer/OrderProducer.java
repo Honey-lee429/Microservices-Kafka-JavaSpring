31d6a6c2f5abab413eb5a8a8e19ccdf215bf91d2
Hanna Taniguchi <hanely.menezes@gmail.com>
2022-12-22 20:49:17 -0300

    call back producer

--- kafka-ms-order/src/main/java/com/course/kafka/broker/producer/OrderProducer.java
+++ kafka-ms-order/src/main/java/com/course/kafka/broker/producer/OrderProducer.java
@@ -2,6 +2,7 @@ package com.course.kafka.broker.producer;
 
 import com.course.kafka.broker.message.OrderMessage;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,21 +12,22 @@ import org.springframework.stereotype.Service;
 import org.apache.kafka.clients.producer.Callback;
 
 @Service
-public class OrderProducer {
+public class OrderProducer<T> {
     // we will just do some log for callback
     private static final Logger LOG = LoggerFactory.getLogger(OrderProducer.class);
 
     //var producer = new KafkaProducer<String, OrderMessage>;
 
-    @Autowired
-    private KafkaProducer<String, OrderMessage> kafkaProducer;
+
+    private KafkaProducer<String, T> kafkaProducer;
 
     @Autowired
     private KafkaTemplate<String, OrderMessage> kafkaTemplate;
 
 
-    public void publish(OrderMessage message) {
-        kafkaProducer.send(, (data,ex) -> {
+    public void publish(OrderMessage message, String topic, String key, String value) {
+        var record = new ProducerRecord<>(topic, key, value);
+        kafkaProducer.send((ProducerRecord<String, T>) record, (data, ex) -> {
             if (ex != null) {
                 ex.printStackTrace();
                 return;
